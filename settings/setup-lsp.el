75fd1c48a52ae7aa1984ae16e9297bdd0680fece
ancurel <github@webmixers.nl>
2018-12-21 10:53:39 +0100

    Added lsp-ui

--- settings/setup-lsp.el
+++ settings/setup-lsp.el
@@ -2,11 +2,13 @@
 ;; Setup lsp mode
 ;; -----------------------------------------
 ;; lsp-mode          | https://github.com/emacs-lsp/lsp-mode
+;; lsp-ui            | https://github.com/emacs-lsp/lsp-ui
 
 (use-package lsp-mode
   :diminish lsp-mode)
 
-(use-package lsp-ui)
+;; (use-package lsp-ui
+;;   :hook (lsp-mode . lsp-ui-mode))
 
 
 (provide 'setup-lsp)
