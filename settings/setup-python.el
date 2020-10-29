1ff2bc84078e4b4ccfb37bc06861215c31fb86cb
ancurel <github@webmixers.nl>
2020-10-29 14:16:47 +0100

    Disabled elpy, enabled lsp-pyright

--- settings/setup-python.el
+++ settings/setup-python.el
@@ -2,19 +2,37 @@
 ;; Setup python 
 ;; -----------------------------------------
 ;; elpy | https://github.com/jorgenschaefer/elpy 
+;; lsp-pyright | https://emacs-lsp.github.io/lsp-pyright/
 
 
-(use-package elpy
-  :mode "\\.py\\'"
+;; (use-package elpy
+;;   :mode "\\.py\\'"
+;;   :config
+;;   (elpy-enable)
+;;   (defalias 'workon 'pyvenv-workon)
+
+;;   (setq elpy-rpc-backend "jedi")
+;;   (when (executable-find "ipython") 
+;;     (setq python-shell-interpreter "ipython"
+;;           python-shell-interpreter-args "-i --simple-prompt")
+;;     ) 
+
+;;   (defvar python-extra-keywords
+;;     '(
+;;       ("\\(return\\)" 0 'font-lock-return-face prepend)
+;;       ))
+
+;;   (font-lock-add-keywords 'python-mode python-extra-keywords)
+
+;;   (add-hook 'python-mode-hook #'electric-operator-mode))
+
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp)))
   :config
-  (elpy-enable)
-  (defalias 'workon 'pyvenv-workon)
-
-  (setq elpy-rpc-backend "jedi")
-  (when (executable-find "ipython") 
-    (setq python-shell-interpreter "ipython"
-          python-shell-interpreter-args "-i --simple-prompt")
-    ) 
+  (add-hook 'python-mode-hook #'electric-operator-mode)
 
   (defvar python-extra-keywords
     '(
@@ -22,8 +40,7 @@
       ))
 
   (font-lock-add-keywords 'python-mode python-extra-keywords)
-
-  (add-hook 'python-mode-hook #'electric-operator-mode))
+  ) 
 
 
 
