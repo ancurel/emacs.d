4582f6dcf0dcd560b18785e047a789938eba655b
ancurel <github@webmixers.nl>
2020-10-29 14:17:56 +0100

    Updated rust config

--- settings/setup-rust.el
+++ settings/setup-rust.el
@@ -2,24 +2,25 @@
 ;; Setup 
 
 ;; Other packages:
-;;    rustic | https://github.com/brotzeit/rustic 
+;;    rust-mode | https://github.com/rust-lang/rust-mode
 ;;    company-racer | https://github.com/emacs-pe/company-racer
 ;;    flycheck-rust | https://github.com/flycheck/flycheck-rust
 
-(use-package rustic
+(use-package rust-mode
   :mode "\\.rs\\'"
   :config
-  ;; (use-package flycheck-rust
-  ;;   :init
-  ;;   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+  ;; (use-package company-racer)
+  (use-package flycheck-rust
+    :init
+    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
   
+  (electric-indent-mode +1)
+
   (add-hook 'rust-mode-hook #'electric-operator-mode)
-  
+
   (after-load 'smartparens
     (sp-local-pair 'rust-mode "'" nil :actions nil))
 
-  (electric-indent-mode +1)
-  (electric-operator-mode +1)
   )
 
 
